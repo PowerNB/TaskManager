7996e0e39fa31762aa4aa8d433bccbc8508588d1
PowerNB <iloginovandreyka@gmail.com>
2026-04-30 11:44:50 +0300

    chore: logger struc

--- src/logger.ts
+++ src/logger.ts
@@ -1,10 +1,22 @@
 import "dotenv/config";
 import pino from "pino";
 
+let transport: pino.TransportSingleOptions | undefined;
+
+if (process.env.NODE_ENV !== "production") {
+    transport = {
+        target: "pino-pretty",
+        options: {
+            colorize: true,
+            translateTime: "SYS:standard",
+            ignore: "pid,hostname",
+        },
+    };
+}
+
 export const logger = pino({
     level: process.env.LOG_LEVEL ?? "info",
-    transport:
-        process.env.NODE_ENV !== "production"
-            ? { target: "pino-pretty", options: { colorize: true } }
-            : undefined,
+    transport,
 });
+
+export type Logger = typeof logger;
